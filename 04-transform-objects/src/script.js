641fb8a36cf49d74a255c5ced661f68aed835f69
Thomas Miller <thomas.miller147@gmail.com>
2024-01-17 23:13:23 +0100

    groups

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -7,22 +7,35 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+scene.add(group)
 
-mesh.position.set(0.7, -0.6, 1)
-mesh.scale.set(2, 0.5, 0.5)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+group.add(cube2)
+cube2.position.x = -2
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+group.add(cube3)
+cube3.position.x = 2
+
+group.position.y = 1
+group.scale.y = 2
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper()
 scene.add(axesHelper)
-
-// Rotation
-mesh.rotation.reorder('YXZ')
-mesh.rotation.x = Math.PI * 0.25
-mesh.rotation.y = Math.PI * 0.25
 
 // Sizes
 const sizes = {
@@ -34,6 +47,7 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
